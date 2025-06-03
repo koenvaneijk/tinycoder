e597958300398517b5ce962368f16b231e838024
Koen van Eijk <vaneijk.koen@gmail.com>
2025-06-03 14:57:03 +0200

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setuptools.setup(
     name="tinycoder",
-    version="0.4.4",
+    version="0.4.5",
     author="Koen van Eijk",
     author_email="vaneijk.koen@gmail.com",
     description="A simplified AI coding assistant.",
